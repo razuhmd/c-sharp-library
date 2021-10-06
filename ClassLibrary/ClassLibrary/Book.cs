bbe0697fa077c84f598a14f411e1df968fda0588
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-10-06 22:23:01 +0200

    C# Library

---
+++ ClassLibrary/ClassLibrary/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public string ISBN13 { get; set; }
+    }
+}
